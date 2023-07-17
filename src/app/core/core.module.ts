c74da6a134d503bc82bff20be35cee5f041591b4
Inamulhaq Inamdar <inam12507@gmail.com>
2023-07-17 23:01:52 +0530

    Creating a boot screen

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { BootComponent } from './boot/boot.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [BootComponent],
+  imports: [CommonModule, SharedModule],
 })
-export class CoreModule { }
+export class CoreModule {}
