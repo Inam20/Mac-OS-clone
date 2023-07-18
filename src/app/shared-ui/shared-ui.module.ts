9c41d89efda4db58ab4f64a85dfd06f3e778f1f5
Inamulhaq Inamdar <inam12507@gmail.com>
2023-07-18 14:34:34 +0530

    Creating a Login screen

--- src/app/shared-ui/shared-ui.module.ts
+++ src/app/shared-ui/shared-ui.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProgressBarModule } from 'primeng/progressbar';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [ProgressBarModule],
+  exports: [ProgressBarModule, InputTextModule],
 })
 export class SharedUiModule {}
